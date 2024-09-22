5202b40390796fdb1b1df5b500f00dcb1746e2bb
Benny Dabush <bennydabush@gmail.com>
2024-09-22 08:42:58 +0300

    connect to mock atlas

--- DB/mongodb/connectToAtlas.js
+++ DB/mongodb/connectToAtlas.js
@@ -4,7 +4,9 @@ const connectionStringForAtlas = "";
 
 const connectToAtlaslDb = async () => {
   try {
-    //await mongoose.connect(connectionStringForAtlas);
+    await mongoose.connect(
+      "mongodb+srv://tzachd1232:Abc123@businesscardappprod.cunmyjr.mongodb.net/?retryWrites=true&w=majority&appName=BusinessCardAppProd"
+    );
     console.log("Connected to MongoDB in atlas");
   } catch (error) {
     console.error("Could not connect to MongoDB", error);
